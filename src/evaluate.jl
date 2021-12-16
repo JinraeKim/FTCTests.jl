5f9a8afa72f81a2d49cb2648d201eeae01cb860d
Hanna Lee <dlgkssk707@gmail.com>
2021-12-16 16:15:16 +0900

    export errors from traj.jld2

--- src/evaluate.jl
+++ src/evaluate.jl
@@ -1,35 +1,34 @@
-# using FaultTolerantControl
-# const FTC = FaultTolerantControl
-# using UnPack
-# using Transducers
+using FTCTests
 using DataFrames
 using JLD2, FileIO
-using Statistics
-using DifferentialEquations
-# using LinearAlgebra
-# using NumericalIntegration
-# using ReferenceFrameRotations
+using Transducers
+using UnPack
+# using Statistics
+# using DifferentialEquations
 
 
 function export_errors(dir_log)
     file_path = joinpath(dir_log, "traj.jld2")
     loaded_data = JLD2.load(file_path)
-    # df = loaded_data
-    # ts = df.time
-    # poss = df.sol |> Map(datum -> datum.plant.state.p) |> collect
-    # zs = poss |> Map(pos -> pos[3]) |> collect
-    # poss_desired = ts |> Map(pos_cmd_func) |> collect
-    # zs_des = poss_desired |> Map(pos -> pos[3]) |> collect
-    # z_error = zs - zs_des
-    # z_error_max = max(z_error)
+    @unpack df, method = loaded_data
+    θs = [[0, 0, -10.0]]  # constant position tracking
+    tf = 20.0
+    pos_cmd_func = Bezier(θs; tf=tf)
+    ts = df.time
+    poss = df.sol |> Map(datum -> datum.plant.state.p) |> collect
+    zs = poss |> Map(pos -> pos[3]) |> collect
+    poss_desired = ts |> Map(pos_cmd_func) |> collect
+    zs_des = poss_desired |> Map(pos -> pos[3]) |> collect
+    z_error = zs - zs_des
+    z_error_max = maximum(z_error)
 end
 
-function evaluate(errors, threshold)
-    @assert threshold > 0
-    mean(abs.((errors) .<= threshold))
-end
+# function evaluate(errors, threshold)
+#     @assert threshold > 0
+#     mean(abs.((errors) .<= threshold))
+# end
 
-function main(dir_log="test/data/adaptive/0001")
+function main(dir_log="data/0001")
     # errors = [-1; 2; 0]  # for test
     errors = export_errors(dir_log)
     # threshold = 1.0

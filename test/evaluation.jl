c7649a918c69d9277236017ee0afaa090aa7696d
Hanna Lee <dlgkssk707@gmail.com>
2021-12-18 21:58:32 +0900

    Update test/evaluation.jl
    Co-authored-by: Jinrae Kim <kjl950403@gmail.com>

--- test/evaluation.jl
+++ test/evaluation.jl
@@ -3,7 +3,7 @@ using FTCTests
 
 
 function main(dir_log="data/0001")
-    file_path = joinpath(dir_log, "traj.jld2")
+    file_path = joinpath(dir_log, FTCTests.TRAJ_DATA_NAME)
     # errors = [-1; 2; 0]  # for test
     errors = FTCTests.export_errors(file_path)
     # threshold = 1.0

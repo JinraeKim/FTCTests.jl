2418433665346621ed9cff215bf0a1dd6d1e6d45
Hanna Lee <dlgkssk707@gmail.com>
2021-12-19 16:18:36 +0900

    Compute recovery rates for several traj.
    Update test/evaluation.jl
    
    Co-authored-by: Jinrae Kim <kjl950403@gmail.com>
    
    Update test/evaluation.jl
    
    Co-authored-by: Jinrae Kim <kjl950403@gmail.com>
    
    Update test/evaluation.jl
    
    Co-authored-by: Jinrae Kim <kjl950403@gmail.com>

--- test/evaluation.jl
+++ test/evaluation.jl
@@ -1,20 +1,29 @@
 using FTCTests
 
 
-function main(N)
+"""
+# Notes
+- (`manoeuvre`) :hovering, :forward, etc.
+- (`method`) :adaptive, :adaptive2optim, etc.
+"""
+function main(manoeuvre::Symbol=:hovering, method::Symbol=:adaptive; _dir_log="data")
+    println("(manoeuvre: $(manoeuvre), method: $(method)")
     _dir_log = "data"
-    method = :adaptive
-    manoeuvre = :hovering
-    dir_log = joinpath(joinpath(_dir_log, String(method)), String(manoeuvre))
     time_criterion = 15.0
     threshold = 1.0
     errors = []
-    for i in 1:N
-        __dir_log = joinpath(dir_log, lpad(string(i), 4, '0'))
-        file_path = joinpath(__dir_log, FTCTests.TRAJ_DATA_NAME)
-        single_error = FTCTests.export_error_max(file_path, time_criterion)
-        push!(errors, single_error)
+    dir_log = joinpath(joinpath(_dir_log, String(manoeuvre)), String(method))
+    while true
+        file_path = joinpath(dir_log, lpad(string(case_number), 4, '0') * "_" * FTCTests.TRAJ_DATA_NAME)
+        if isfile(file_path)
+            single_error = FTCTests.export_error_max(file_path, time_criterion)
+            push!(errors, single_error)
+            case_number += 1
+        else
+            break
+        end
     end
-    @show errors
+    # @show errors, case_number
     recovery_rate = 100 * FTCTests.evaluate(errors, threshold)
+    println("Recovery rate is: $(recovery_rate) percent.")
 end

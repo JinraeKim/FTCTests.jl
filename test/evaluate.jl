6d6237899ada2ff394261c84a666a22ffb60f1eb
Hanna Lee <dlgkssk707@gmail.com>
2021-12-20 15:50:07 +0900

    Remove unnecessary lines

--- test/evaluate.jl
+++ test/evaluate.jl
@@ -10,14 +10,12 @@ function main(manoeuvre::Symbol=:hovering, method::Symbol=:adaptive; _dir_log="d
     println("(manoeuvre: $(manoeuvre), method: $(method))")
     time_criterion = 15.0
     threshold = 1.0
-    results = []
     dir_log = joinpath(joinpath(_dir_log, String(manoeuvre)), String(method))
-    for case_number in 1:length(readdir(dir_log))
-        file_path = joinpath(dir_log, lpad(string(case_number), 4, '0') * "_" * FTCTests.TRAJ_DATA_NAME)
-        result, error_max = FTCTests.evaluate(file_path, time_criterion, threshold)
-        push!(results, result)
-    end
-    if results[1] == true
+    case_number = 1
+    file_path = joinpath(dir_log, lpad(string(case_number), 4, '0') * "_" * FTCTests.TRAJ_DATA_NAME)
+    result, error_max = FTCTests.evaluate(file_path, time_criterion, threshold)
+
+    if result == true
         println("Success.")
     else
         println("Fail.")

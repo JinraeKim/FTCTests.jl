14fc054ed1b61b91cd5fa9c491c9e90501d046a2
JinraeKim <kjl950403@gmail.com>
2022-01-04 17:10:09 +0900

    Add compute costs (template)

---
+++ main/compute_costs.jl
@@ -0,0 +1,27 @@
+using FTCTests
+using JLD2
+using Transducers
+
+
+function compute_cost(file_path::String; cost=PositionAngularVelocityCostFunctional())
+    jld2 = JLD2.load(file_path)
+    @unpack df, traj_des = jld2
+    ts = df.time
+    poss = df.sol |> Map(datum -> datum.plant.state.p) |> collect
+    poss_des = ts |> Map(traj_des) |> collect
+    e_ps = poss .- poss_des
+    @warn("TODO: there is no desired angular velocity")
+    e_ωs = ts |> Map(t -> zeros(3)) |> collect
+    (;
+     ts=ts,
+     e_ps=e_ps,
+     e_ωs=e_ωs,
+    )
+    J = cost(ts, e_ps, e_ωs)
+end
+
+function compute_costs(; dir_log="data/hovering/adaptive")
+    @warn("TODO: change the directory location for your purpose")
+    file_paths = readdir(dir_log; join=true)
+    Js = file_paths |> Map(compute_cost) |> collect
+end

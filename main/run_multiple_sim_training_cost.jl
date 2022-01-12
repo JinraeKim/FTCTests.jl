97af8cbd836f9be4ffb79be6cddde72ce0a96407
JinraeKim <kjl950403@gmail.com>
2022-01-12 18:46:04 +0900

    Update traj data generation code

--- main/run_multiple_sim_training_cost.jl
+++ main/run_multiple_sim_training_cost.jl
@@ -8,7 +8,7 @@ using Test
 This is for the training of cost function.
 """
 function run_multiple_sim_training_cost(N=1;
-        manoeuvres=[:debug],
+        manoeuvres=[:hovering],
         methods=[:adaptive],
         t0=0.0, tf=20.0,
         h_threshold=nothing,  # m (nothing: no constraint)
@@ -21,8 +21,11 @@ function run_multiple_sim_training_cost(N=1;
     # rotor index ∈ {1, 2, 3, 4, 5, 6}
     # effectiveness ∈ [0, 1]
     _faults = 1:N |> Map(i -> [LoE(fault_time, rand(1:6), rand(1)[1])]) |> collect  # randomly sampled N faults
-    θs = [[0, 0, -9.0]]  # Control points of Bezier curve; constant position tracking
-    θs_array = 1:N |> Map(i -> θs) |> collect
+    # for trajectory parameters (terminal position constraint)
+    multicopter = LeeHexacopter()  # dummy
+    min_nt, max_nt = FTCTests.distribution_info(:hovering)
+    # θs = [[0, 0, -10.0]]  # Control points of Bezier curve; constant position tracking
+    θs_array = 1:N |> Map(i -> [FTCTests.sample(multicopter, min_nt, max_nt)[1]]) |> collect  # sample(...)[1] corresponds to the position vector
     run_multiple_sim(N,
                      manoeuvres,
                      methods,

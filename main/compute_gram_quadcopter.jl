2c51f18f5649e7ed83cae3c98ed5304e8b1e7acf
JinraeKim <kjl950403@gmail.com>
2022-03-18 17:11:02 +0900

    Fix bug; add effectiveness

--- main/compute_gram_quadcopter.jl
+++ main/compute_gram_quadcopter.jl
@@ -9,122 +9,126 @@ using ReferenceFrameRotations
 
 function Dynamics!(multicopter::IslamQuadcopter)
     @unpack B = multicopter
-    function dynamics!(dx, x, param, t; u)
-        ν = B * u
+    function dynamics!(dx, x, param, t; u, Λ)
+        ν = B * Λ * u
         f, M = ν[1], ν[2:4]
         @nested_log FSimZoo.__Dynamics!(multicopter)(dx, x, (), t; f=f, M=M)
     end
 end
 
 
-"""
-# Refs
-[1] M. Tahavori and A. Hasan, “Fault recoverability for nonlinear systems with application to fault tolerant control of UAVs,” Aerosp. Sci. Technol., vol. 107, p. 106282, 2020, doi: 10.1016/j.ast.2020.106282.
+# """
+# # Refs
+# [1] M. Tahavori and A. Hasan, “Fault recoverability for nonlinear systems with application to fault tolerant control of UAVs,” Aerosp. Sci. Technol., vol. 107, p. 106282, 2020, doi: 10.1016/j.ast.2020.106282.
 
-# Notes
-- x = state vector, [eta, omega, v, xi]^T
-    - eta = Euler angle vector, [phi, theta, psi]^T
-    - omega = angular rate vector, [p, q, r]^T
-    - v = velocity vector, [u, v, w]^T
-    - xi = position vector, [x, y, z]^T
-- u = control input vector, thrust vector, [omega_1, omega_2, omega_3, omega_4]^T
-- y = output vector, [eta, omega, xi]^T
-"""
-function compute_minHSV_deprecated(lambda, num=1)
-    # Quadcopter state space model [1]
-    gc = 9.81
-    m = 0.65
-    l = 0.023
-    k = 31.1e-5
-    b = 7.5e-7
-    J = Diagonal([7.5e-3, 7.4e-3, 1.3e-2])
-    Jinv = inv(J)
-    A = [zeros(3, 3) Matrix(I, 3, 3) zeros(3, 3) zeros(3, 3);
-    zeros(3, 3) zeros(3, 3) zeros(3, 3) zeros(3, 3);
-    zeros(3, 3) zeros(3, 3) zeros(3, 3) zeros(3, 3);
-    zeros(3, 3) zeros(3, 3) Matrix(I, 3, 3) zeros(3, 3)]
-    B = zeros(12, 4)
-    for i = 1:12
-        for j = 1:4
-            if i == 5
-                if j == 1
-                    B[i, j] = -l*k/J[2, 2]
-                elseif j == 3
-                    B[i, j] = l*k/J[2, 2]
-                end
-            elseif i == 4
-                if j == 2
-                    B[i, j] = -l*k/J[1, 1]
-                elseif j == 4
-                    B[i, j] = l*k/J[1, 1]
-                end
-            elseif i == 6
-                if j == 1 || j == 3
-                    B[i, j] = -b/J[3, 3]
-                elseif j == 2 || j == 4
-                    B[i, j] = b/J[3, 3]
-                end
-            elseif i == 9
-                if j == 1 || j == 2 || j == 3 || j == 4
-                    B[i, j] = k/m
-                end
-            else
-                B[i, j] = 0
-            end
-        end
-    end
-    C = [Matrix(I, 3, 3) zeros(3, 3) zeros(3, 3) zeros(3, 3);
-    zeros(3, 3) Matrix(I, 3, 3) zeros(3, 3) zeros(3, 3);
-    zeros(3, 3) zeros(3, 3) zeros(3, 3) Matrix(I, 3, 3)]
+# # Notes
+# - x = state vector, [eta, omega, v, xi]^T
+#     - eta = Euler angle vector, [phi, theta, psi]^T
+#     - omega = angular rate vector, [p, q, r]^T
+#     - v = velocity vector, [u, v, w]^T
+#     - xi = position vector, [x, y, z]^T
+# - u = control input vector, thrust vector, [omega_1, omega_2, omega_3, omega_4]^T
+# - y = output vector, [eta, omega, xi]^T
+# """
+# function compute_minHSV_deprecated(lambda, num=1)
+#     # Quadcopter state space model [1]
+#     gc = 9.81
+#     m = 0.65
+#     l = 0.023
+#     k = 31.1e-5
+#     b = 7.5e-7
+#     J = Diagonal([7.5e-3, 7.4e-3, 1.3e-2])
+#     Jinv = inv(J)
+#     A = [zeros(3, 3) Matrix(I, 3, 3) zeros(3, 3) zeros(3, 3);
+#     zeros(3, 3) zeros(3, 3) zeros(3, 3) zeros(3, 3);
+#     zeros(3, 3) zeros(3, 3) zeros(3, 3) zeros(3, 3);
+#     zeros(3, 3) zeros(3, 3) Matrix(I, 3, 3) zeros(3, 3)]
+#     B = zeros(12, 4)
+#     for i = 1:12
+#         for j = 1:4
+#             if i == 5
+#                 if j == 1
+#                     B[i, j] = -l*k/J[2, 2]
+#                 elseif j == 3
+#                     B[i, j] = l*k/J[2, 2]
+#                 end
+#             elseif i == 4
+#                 if j == 2
+#                     B[i, j] = -l*k/J[1, 1]
+#                 elseif j == 4
+#                     B[i, j] = l*k/J[1, 1]
+#                 end
+#             elseif i == 6
+#                 if j == 1 || j == 3
+#                     B[i, j] = -b/J[3, 3]
+#                 elseif j == 2 || j == 4
+#                     B[i, j] = b/J[3, 3]
+#                 end
+#             elseif i == 9
+#                 if j == 1 || j == 2 || j == 3 || j == 4
+#                     B[i, j] = k/m
+#                 end
+#             else
+#                 B[i, j] = 0
+#             end
+#         end
+#     end
+#     C = [Matrix(I, 3, 3) zeros(3, 3) zeros(3, 3) zeros(3, 3);
+#     zeros(3, 3) Matrix(I, 3, 3) zeros(3, 3) zeros(3, 3);
+#     zeros(3, 3) zeros(3, 3) zeros(3, 3) Matrix(I, 3, 3)]
 
-    u_ss = sqrt(m*gc/4/k) * ones(4,)
-    T(x) = [-sin(x[2]) 0 1;
-            cos(x[2])*sin(x[3]) cos(x[3]) 0;
-            cos(x[2])*cos(x[3]) -sin(x[3]) 0]
-    R(x) = [cos(x[2])*cos(x[3]) sin(x[3])*sin(x[2]) -sin(x[2]);
-            cos(x[3])*sin(x[2])*sin(x[1])-sin(x[3])*cos(x[1]) sin(x[3])*sin(x[2])*sin(x[1])+cos(x[3])*cos(x[1]) cos(x[2])*sin(x[1]);
-            cos(x[3])*sin(x[2])*cos(x[1])-sin(x[3])*sin(x[1]) sin(x[3])*sin(x[2])*cos(x[1])+cos(x[3])*sin(x[1]) cos(x[2])*cos(x[1])]
-    F(x) = vec([
-        T(x) * x[4:6]
-        Jinv * cross(-x[4:6], J * x[4:6])
-        -cross(x[4:6], x[7:9]) + gc * R(x)' * [0; 0; 1]
-        R(x) * x[7:9]
-    ])
+#     u_ss = sqrt(m*gc/4/k) * ones(4,)
+#     T(x) = [-sin(x[2]) 0 1;
+#             cos(x[2])*sin(x[3]) cos(x[3]) 0;
+#             cos(x[2])*cos(x[3]) -sin(x[3]) 0]
+#     R(x) = [cos(x[2])*cos(x[3]) sin(x[3])*sin(x[2]) -sin(x[2]);
+#             cos(x[3])*sin(x[2])*sin(x[1])-sin(x[3])*cos(x[1]) sin(x[3])*sin(x[2])*sin(x[1])+cos(x[3])*cos(x[1]) cos(x[2])*sin(x[1]);
+#             cos(x[3])*sin(x[2])*cos(x[1])-sin(x[3])*sin(x[1]) sin(x[3])*sin(x[2])*cos(x[1])+cos(x[3])*sin(x[1]) cos(x[2])*cos(x[1])]
+#     F(x) = vec([
+#         T(x) * x[4:6]
+#         Jinv * cross(-x[4:6], J * x[4:6])
+#         -cross(x[4:6], x[7:9]) + gc * R(x)' * [0; 0; 1]
+#         R(x) * x[7:9]
+#     ])
 
-    if num == 1
-        eff = Diagonal([lambda, 1, 1, 1])
-    elseif num == 2
-        eff = Diagonal([1, lambda, 1, 1])
-    elseif num == 3
-        eff = Diagonal([1, 1, lambda, 1])
-    elseif num == 4
-        eff = Diagonal([1, 1, 1, lambda])
-    else
-        error("Choose fauly actuator number")
-    end
+#     if num == 1
+#         eff = Diagonal([lambda, 1, 1, 1])
+#     elseif num == 2
+#         eff = Diagonal([1, lambda, 1, 1])
+#     elseif num == 3
+#         eff = Diagonal([1, 1, lambda, 1])
+#     elseif num == 4
+#         eff = Diagonal([1, 1, 1, lambda])
+#     else
+#         error("Choose fauly actuator number")
+#     end
 
-    f(x, u, p, t) = A*x + F(x) + B*(eff * u + u_ss).^2
-    g(x, u, p, t) = C*x
+#     f(x, u, p, t) = A*x + F(x) + B*(eff * u + u_ss).^2
+#     g(x, u, p, t) = C*x
 
-	# System dimension
-	n = size(A)[1]
-	m = size(B)[2]
-	l = size(C)[1]
+# 	# System dimension
+# 	n = size(A)[1]
+# 	m = size(B)[2]
+# 	l = size(C)[1]
 
-    # Initial settings
-    x0 = zeros(n,)
-    u0 = zeros(m,)
-    dt = 0.001
-    tf = 1.0
-    pr = zeros(4, 1)
+#     # Initial settings
+#     x0 = zeros(n,)
+#     u0 = zeros(m,)
+#     dt = 0.001
+#     tf = 1.0
+#     pr = zeros(4, 1)
 
-    Wc = FTC.empirical_gramian(f, g, m, n, l; opt=:c, dt=dt, tf=tf, pr=pr, xs=x0, us=u0)
-    Wo = FTC.empirical_gramian(f, g, m, n, l; opt=:o, dt=dt, tf=tf, pr=pr, xs=x0, us=u0)
-	minHSV = FTC.min_HSV(Wc, Wo)
-end
+#     Wc = FTC.empirical_gramian(f, g, m, n, l; opt=:c, dt=dt, tf=tf, pr=pr, xs=x0, us=u0)
+#     Wo = FTC.empirical_gramian(f, g, m, n, l; opt=:o, dt=dt, tf=tf, pr=pr, xs=x0, us=u0)
+# 	minHSV = FTC.min_HSV(Wc, Wo)
+# end
 
-function compute_minHSV(lambda, num::Int; dt=0.01, tf=1.0)
+
+function compute_minHSV_squared(lambda, num::Int; dt=0.001, tf=1.0)
     @assert lambda >= 0.0 && lambda <= 1.0
+    Λ = diagm(ones(4))
+    Λ[num, num] = lambda
+    @show Λ
     multicopter = IslamQuadcopter()
     function f(x, u, param, t)
         p = x[1:3]
@@ -134,7 +138,7 @@ function compute_minHSV(lambda, num::Int; dt=0.01, tf=1.0)
         R = quat_to_dcm(Quaternion(quat...))
         X = State(multicopter)(p, v, R, ω)
         dX = State(multicopter)()  # initialisation
-        Dynamics!(multicopter)(dX, X, param, t; u=u)
+        Dynamics!(multicopter)(dX, X, param, t; u=u, Λ=Λ)
         dX_quat = dcm_to_quat(DCM(dX.R))
         dx = [dX.p..., dX.v..., dX_quat.q0, dX_quat.q1, dX_quat.q2, dX_quat.q3, dX.ω...]
         return dx
@@ -150,20 +154,29 @@ function compute_minHSV(lambda, num::Int; dt=0.01, tf=1.0)
 
     Wc = FTC.empirical_gramian(f, g, m, n, l; opt=:c, dt=dt, tf=tf, pr=pr, xs=x0, us=u0)
     Wo = FTC.empirical_gramian(f, g, m, n, l; opt=:o, dt=dt, tf=tf, pr=pr, xs=x0, us=u0)
-    print(Wc*Wo |> LinearAlgebra.eigvals)
-	minHSV = FTC.min_HSV(Wc, Wo)
+    _eigvals_WcWo = Wc*Wo |> LinearAlgebra.eigvals
+    eigvals_WcWo = []
+    for eigval in _eigvals_WcWo
+        if abs(imag(eigval)) < 1e-6
+            push!(eigvals_WcWo, real(eigval))
+        else
+            error("Too large imaginary part")
+        end
+    end
+    min_HSV_squared = eigvals_WcWo |> minimum  # not min_HSV
+	# minHSV = FTC.min_HSV(Wc, Wo)
 end
 
 function plotting(rotor_idx)
-    lambda = 0:0.10:1 |> collect
-    HSVs = []
+    lambda = 0:0.20:1 |> collect
+    HSV_squareds = []
     for i = 1:length(lambda)
-        HSVs = push!(HSVs, compute_minHSV(lambda[i], rotor_idx))
+        HSV_squareds = push!(HSV_squareds, compute_minHSV_squared(lambda[i], rotor_idx))
     end
     return plot(lambda,
-                HSVs,
+                HSV_squareds,
                 xlabel = "Actuator effectiveness",
-                ylabel = "Minimum HSVs",
+                ylabel = "Minimum HSV_squareds",
                 label = nothing,
                )
 end
